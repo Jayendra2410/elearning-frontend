587b44332610c41921da1809b685493f86fbc220
Jayendra <jayendrasinghjadone@gmail.com>
2025-08-16 14:25:45 +0530

    initial commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+  return (
+    <div className="about">
+        <div className="about-content">
+            <h2>About Us</h2>
+            <p>
+                We are a team of dedicated individuals committed to providing high-quality
+                educational resources and support to students of all ages. Our mission is to
+                empower learners by offering a wide range of courses, interactive tools, and
+                personalized guidance to help them achieve their academic goals.
+            </p>
+        </div>
+    </div>
+  );
+};
+
+export default About;
